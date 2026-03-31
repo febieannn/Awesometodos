2ac28961f56c9cba9f24132e856183ade3a04b87
febieann <mombayfebieann@gmail.com>
2026-03-31 16:09:36 +0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+console.log("Checking DB URI:", process.env.MONGODB_URI);
+const express = require("express");
+const { connectDB } = require("./models/index.js");
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+const router = require("./routes");
+app.use("/api", router);
+
+const port = process.env.PORT || 5000;
+
+const startServer = async() => {
+    await connectDB();
+    app.listen(port, () => {
+        console.log(`Server is listening on http://localhost:${port}`);
+    });
+};
+startServer();
